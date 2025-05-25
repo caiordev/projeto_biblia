444e6e461e37cafb4b82ddc1005b93fbf5b14a94
caiordev <caioreisdev@gmai.com>
2025-05-24 22:29:43 -0300

    feat: teste deploy

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/projeto_biblia/', // Add this line for GitHub Pages deployment
 })
